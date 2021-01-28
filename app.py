29505b616728806ee1aad6dfabebb6b563ab8b35
Alexis Rhote <alexis.rhote@gmail.com>
2021-01-28 15:09:03 -0600

    Get input and output results

---
+++ app.py
@@ -0,0 +1,18 @@
+from num_to_roman.exceptions import OutOfRangeException
+from num_to_roman.roman_numeral import RomanNumeral
+
+while True:
+    """
+    Attempt to get valid input
+    """
+    while True:
+        try:
+            number = int(input("Please enter a number to convert to Roman Numerals: "))
+            break
+        except ValueError:
+            print("That was not a valid number. Please try again.")
+
+    try:
+        print(RomanNumeral(number))
+    except OutOfRangeException as ex:
+        print(ex.message)
